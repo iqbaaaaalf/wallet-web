8cb544bbe9438aa97d68d5d8631aa0ae035f3a71
novitri malini <novitri.malini@gmail.com>
2018-03-09 20:39:00 +0700

    adding handleInputOnSubmit and test case

--- src/__tests__/LoginForm.test.js
+++ src/__tests__/LoginForm.test.js
@@ -1,70 +1,72 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import LoginForm from '../js/LoginForm';
 
 describe('LoginForm', () => {
   describe('#handleInputUsername', () => {
     it('should save name with Budi', () => {
-      const wrapper = shallow(<LoginForm />);
+      const wrapper = shallow(<LoginForm/>);
       const inputUserName = wrapper.find('#username');
-      inputUserName.simulate('change',{
-        target:{
+      inputUserName.simulate('change', {
+        target: {
           value: 'Budi',
         },
       });
       expect(wrapper.state('username')).toBe('Budi');
     });
     it('should save name with Doni', () => {
-      const wrapper = shallow(<LoginForm />);
+      const wrapper = shallow(<LoginForm/>);
       const inputUserName = wrapper.find('#username');
-      inputUserName.simulate('change',{
-        target:{
+      inputUserName.simulate('change', {
+        target: {
           value: 'Doni',
         },
       });
       expect(wrapper.state('username')).toBe('Doni');
     });
-    it('should save name with Correct input', () => {
-      const wrapper = shallow(<LoginForm />);
-      const inputUserName = wrapper.find('#username');
-      inputUserName.simulate('change',{
-        target:{
-          value: 'Doni',
-        },
-      });
-      expect(wrapper.state('username')).not.toBe('Budi');
-    });
   });
   describe('#handleInputPassword', () => {
     it('should save password with 123456', () => {
-      const wrapper = shallow(<LoginForm />);
+      const wrapper = shallow(<LoginForm/>);
       const inputPassword = wrapper.find('#password');
-      inputPassword.simulate('change',{
-        target:{
-          value:'123456',
+      inputPassword.simulate('change', {
+        target: {
+          value: '123456',
         },
       });
       expect(wrapper.state('password')).toEqual('123456');
     });
     it('should save password with 12000', () => {
-      const wrapper = shallow(<LoginForm />);
+      const wrapper = shallow(<LoginForm/>);
       const inputPassword = wrapper.find('#password');
-      inputPassword.simulate('change',{
-        target:{
-          value:'12000',
+      inputPassword.simulate('change', {
+        target: {
+          value: '12000',
         },
       });
       expect(wrapper.state('password')).toEqual('12000');
     });
-    it('should save password with correct value', () => {
-      const wrapper = shallow(<LoginForm />);
-      const inputPassword = wrapper.find('#password');
-      inputPassword.simulate('change',{
-        target:{
-          value:'12000',
-        },
-      });
-      expect(wrapper.state('password')).not.toEqual('13000');
+  });
+  describe('#handleInputOnSubmit', () => {
+    it('should call callback after submit button', () => {
+      const mockFunction = jest.fn();
+      const wrapper = mount(<LoginForm onSubmit={mockFunction}/>);
+      const submit = wrapper.find('#submit');
+      submit.simulate('click');
+      expect(mockFunction).toHaveBeenCalled();
+    });
+    it('should call callback with given data', () => {
+      const mockData = {
+        username: 'iqbaaaaalf',
+        password: 'asdasd',
+      };
+      const mockFunction = jest.fn();
+      const wrapper = mount(<LoginForm onSubmit={mockFunction}/>);
+      const submit = wrapper.find('#submit');
+      wrapper.setState(mockData);
+      submit.simulate('click');
+      expect(mockFunction).toHaveBeenCalled();
+      expect(mockFunction).toHaveBeenLastCalledWith(mockData);
     });
   });
 });
--- src/js/LoginForm.js
+++ src/js/LoginForm.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 
 export default class LoginForm extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
     };
     this._handleInputUsername = this._handleInputUsername.bind(this);
     this._handleInputPassword = this._handleInputPassword.bind(this);
+    this._handleInputOnSubmit = this._handleInputOnSubmit.bind(this);
   }
-  _handleInputUsername(event){
+
+  _handleInputUsername(event) {
     this.setState({
-      username: event.target.value
+      username: event.target.value,
     });
   }
-  _handleInputPassword(event){
+
+  _handleInputPassword(event) {
     this.setState({
-      password: event.target.value
+      password: event.target.value,
     });
   }
-  render(){
-    const { username , password} = this.state;
-    return(
+
+  _handleInputOnSubmit() {
+    const { username, password } = this.state;
+    const data = {
+      username: username,
+      password: password,
+    };
+    this.props.onSubmit(data);
+  }
+
+  render() {
+    const { username, password } = this.state;
+    return (
         <section>
           <label htmlFor="username">username:</label>
           <input type="text" id="username" onChange={this._handleInputUsername} value={username}/>
           <label htmlFor="password">username:</label>
           <input type="text" id="password" onChange={this._handleInputPassword} value={password}/>
+          <button id="submit" onClick={this._handleInputOnSubmit}>Login</button>
         </section>
     );
   }
