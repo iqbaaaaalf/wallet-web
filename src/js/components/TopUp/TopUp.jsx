6a564d17e34ff1068aa8638b5ab3d9ae154d44ff
novitri malini <novitri.malini@gmail.com>
2018-03-13 16:06:22 +0700

    adding styling to TopUp

--- src/js/components/TopUp/TopUp.jsx
+++ src/js/components/TopUp/TopUp.jsx
@@ -35,12 +35,27 @@ export default class TopUp extends Component{
   render(){
     const { amount } = this.state;
     return(
-        <div>
-          <label>Balance : </label>
-          <p className="balance">{this.state.balance}</p>
-          <label htmlFor="amount">Amount: </label>
-          <input type="text" className="amountTopUp" onChange={this._handleAmountTopUp} placeholder="ex:50000" value={amount}/>
-          <button type="submit" className="submit" onClick={this._handleSubmit}>Top Up</button>
+        <div className="card border-primary mb-3 col-4 align-content-lg-center">
+          <div className="card-header">Top Up Wallet</div>
+          <div className="card-body text-primary">
+            <div className="row">
+              <div className="col">
+                <label htmlFor="wallet">Wallet : </label>
+              </div>
+              <div className="col-9">
+                <p className="Wallet">{this.state.balance}</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <label htmlFor="amount">Amount: </label>
+              </div>
+              <div className="col-9">
+                <input type="text" className="amountTopUp" onChange={this._handleAmountTopUp} placeholder="ex:50000" value={amount}/>
+              </div>
+            </div>
+            <button type="submit" className="submit" onClick={this._handleSubmit}>Top Up</button>
+          </div>
         </div>
     );
   }
