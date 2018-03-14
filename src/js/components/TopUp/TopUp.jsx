a110ac680496a22866ca544430c589d29ea612e6
Eddo Careera Iriyanto Putra <eddocareera.i.p@gmail.com>
2018-03-14 11:12:35 +0700

    update component TopUp

--- src/js/components/TopUp/TopUp.jsx
+++ src/js/components/TopUp/TopUp.jsx
@@ -1,60 +1,79 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
-export default class TopUp extends Component{
+export default class TopUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       amount: '',
-      errorInvalid: '',
-      balance : '15000',
+      message: '',
+      userId: '',
     };
     this._handleAmountTopUp = this._handleAmountTopUp.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
   }
 
-  _handleAmountTopUp(event){
+  _handleAmountTopUp(event) {
     this.setState({
       amount: event.target.value,
     });
   }
 
-  _handleSubmit(){
-    const { amount } = this.state;
-    const data = {
-      amount: amount,
-    };
-
-    if ( amount === ''){
+  _handleSubmit() {
+    const { amount, userId } = this.state;
+    let valid = true;
+    if (amount === '') {
+      valid = false;
       this.setState({
-        errorInvalid: 'Invalid amount'
+        message: 'Amount Required',
       });
     }
-    this.props.onSubmit(data);
+
+    if (amount !== '' && Number(amount) <= 0) {
+      valid = false;
+      this.setState({
+        message: 'Invalid Amount',
+      });
+    }
+
+    if (valid) {
+      axios.post(`http://localhost:3000/users/${userId}/wallets`, {
+        amount: Number(amount),
+      }).then((response) => {
+        this.setState({
+          message: 'Success',
+        });
+      }).catch((e) => {
+        this.setState({
+          message: e.response.data.message,
+        });
+      });
+    }
   }
 
-  render(){
-    const { amount } = this.state;
-    return(
-        <div className="card border-primary mb-3 align-content-lg-center">
-          <div className="card-header">Top Up Wallet</div>
-          <div className="card-body text-primary">
-            <div className="row">
-              <div className="col">
-                <label htmlFor="wallet">Wallet : </label>
+  render() {
+    const { amount, message } = this.state;
+    return (
+        <div className={'container'}>
+          <div className="card border-primary mb-3 align-content-lg-center">
+            <div className="card-header">Top Up Wallet</div>
+            <div className="card-body text-primary">
+              <div className="row">
+                <div className="col">
+                  {message !== '' ? <p>{message}</p> : <p></p>}
+                </div>
               </div>
-              <div className="col-9">
-                <p className="Wallet">{this.state.balance}</p>
+              <div className="row">
+                <div className="col">
+                  <input type="number" id="amount" onChange={this._handleAmountTopUp}
+                         placeholder="ex:50000"
+                         value={amount} />
+                </div>
               </div>
+              <button type="submit" id="submit" className="submit" onClick={this._handleSubmit}>Top
+                Up
+              </button>
             </div>
-            <div className="row">
-              <div className="col">
-                <label htmlFor="amount">Amount: </label>
-              </div>
-              <div className="col-9">
-                <input type="text" className="amountTopUp" onChange={this._handleAmountTopUp} placeholder="ex:50000" value={amount}/>
-              </div>
-            </div>
-            <button type="submit" className="submit" onClick={this._handleSubmit}>Top Up</button>
           </div>
         </div>
     );
