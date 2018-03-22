673c9ed382d13d4a30d4247c8d1a87f17edeb462
Iqbal Fakhri <iqbaaaaalf@apps.ipb.ac.id>
2018-03-22 11:39:41 +0700

    refactor using image for run and test

--- build.groovy
+++ build.groovy
@@ -1,11 +1,17 @@
-node(){
-  stage('pull repo'){
-    git 'https://github.com/iqbaaaaalf/wallet-web.git'
-  }
-  stage('build artifact'){
-    sh "docker build -t walletweb ."
-  }
-  stage('deploy'){
-    sh "docker run -dit -p 3000:3000 walletweb"
-  }
+node {
+    def dockerfile = 'Dockerfile'
+    def testImage = docker.build("wallet-web", "-f ${dockerfile} .")
+    stage('Build'){
+        testImage.inside{
+            sh "npm install"
+        }
+    }
+    stage ("Test") {
+        testImage.inside{
+            sh "npm run test"
+        }
+    }
+    stage('deploy'){
+      sh "npm start"
+    }
 }
