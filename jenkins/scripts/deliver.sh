47359edb33a53f8fa0558889cb389e211346e37f
Iqbal Fakhri <iqbaaaaalf@apps.ipb.ac.id>
2018-03-22 13:59:51 +0700

    coba ganti script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,1 +1,5 @@
+set -x
 npm start
+sleep 1
+echo $! > .pidfile
+set +x
--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,1 +1,3 @@
-ps -ef | grep test.sh | grep -v grep | awk '{print $2}' | xargs kill
+#ps -ef | grep test.sh | grep -v grep | awk '{print $2}' | xargs kill
+set -x
+kill ${cat .pidfile}
